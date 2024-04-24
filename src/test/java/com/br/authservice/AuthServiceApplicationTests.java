13607a0b525876d9d6a8412f49c181031f13cc40
Thiago Tierre <thiagotierre.lima@gmail.com>
2024-04-24 18:31:48 -0300

    first commit

---
+++ src/test/java/com/br/authservice/AuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.authservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
